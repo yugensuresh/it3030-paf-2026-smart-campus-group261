2ffd6f411d9a30521d54df66ac9e241f793d6432
Yugen Suresh <yugen20030711@gmail.com>
2026-04-19 07:33:03 +0530

    Add basic application test
    Include a simple Spring Boot context load test.

---
+++ src/test/java/campus_nexus/It3030Paf2026SmartCampusApplicationTests.java
@@ -0,0 +1,13 @@
+package campus_nexus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class It3030Paf2026SmartCampusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
