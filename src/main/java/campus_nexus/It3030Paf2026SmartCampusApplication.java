cc1312ee947b73bf1612e4d53fd3f2898720aef7
Yugen Suresh <yugen20030711@gmail.com>
2026-04-19 06:58:47 +0530

    Add Spring Boot application entry point
    Set up the main application bootstrap class.

---
+++ src/main/java/campus_nexus/It3030Paf2026SmartCampusApplication.java
@@ -0,0 +1,13 @@
+package campus_nexus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class It3030Paf2026SmartCampusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(It3030Paf2026SmartCampusApplication.class, args);
+	}
+
+}
